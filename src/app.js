6cb7fa15524c6655903777bc71aefcb68093190d
Katalin Juhász <juhaszkatalin@ktch.hu>
2023-02-14 23:57:13 +0100

    Elkészítés

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// Juhász Katalin, SZOFT I-1_E, 2023-02-14
+
+const aElem = document.querySelector('#a');
+const bElem = document.querySelector('#b');
+const cElem = document.querySelector('#c');
+const calcButton = document.querySelector('#calcButton');
+const areaElem = document.querySelector('#area');
+
+calcButton.addEventListener('click', () => {
+    
+    let a = Number(aElem.value);
+    let b = Number(bElem.value);
+    let c = Number(cElem.value);
+    let area = calcArea(a, b, c);
+    console.log(area);
+    areaElem.value = area;
+
+});
+
+function calcArea(a, b, c) {
+    let area = a*b*c;
+    return area;
+}
